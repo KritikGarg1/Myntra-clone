272edafb273c3eaefdc8070dacb657abe608e219
Kritik Garg <kritikgarg123@gmail.com>
2020-06-23 12:55:36 +0530

    First

---
+++ android/app/src/main/kotlin/com/kdev/myntraclone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kdev.myntraclone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body:  SafeArea(
+          child: WebView(
+                      initialUrl: 'https://www.myntra.com/',
+                      javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+      _controller.complete(webViewController);},
+          ),
+        ),
+      ),
+    );
+  }
+}
